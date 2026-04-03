ad1205acffbefa97e72047e8fd912f24d43ea433
junhyung.lee <lee.junhyung@outlook.com>
2026-04-03 12:47:37 +0900

    fix: align release download paths with versioned asset

---
+++ src/config/release.ts
@@ -0,0 +1,4 @@
+export const RELEASE_TAG = 'v0.1.0';
+export const RELEASE_ASSET = 'CLIX-0.1.0.zip';
+export const RELEASE_DOWNLOAD_URL =
+  `https://github.com/junhyung-space/clix-web/releases/download/${RELEASE_TAG}/${RELEASE_ASSET}`;
